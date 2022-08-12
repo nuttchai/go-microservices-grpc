d0dc0540dc5fad075e6477ea32bf8b274714843f
nuttchai <nuttchai@outlook.com>
2022-08-12 14:21:07 +0700

    Unary API Client Implementation

--- greet/client/main.go
+++ greet/client/main.go
@@ -5,6 +5,8 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/nuttchai/go-microservices-grpc/greet/proto"
 )
 
 var addr string = "localhost:50051"
@@ -16,4 +18,7 @@ func main() {
 		log.Fatalf("Failed to connect: %v\n", err)
 	}
 	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+	doGreet(client)
 }
